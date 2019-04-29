72b9ad41ac251680f8faf038f876b592456cdd73
adam ruyten <aruyten@gmail.com>
2019-04-28 20:10:20 -0400

    added sql file to add values to database

---
+++ SQL insertion queries.sql
@@ -0,0 +1,31 @@
+insert into inventory VALUES(0001, 'pencil', 2.99, 100);
+insert into inventory VALUES(0002, 'pen', 3.99, 100);
+insert into inventory VALUES(0003, 'eraser', 0.99, 50);
+insert into inventory VALUES(0004, 'ruler', 1.99, 175);
+insert into inventory VALUES(0005, 'marker', 4.99, 28);
+insert into inventory VALUES(0006, 'crayon', 2.99, 85);
+insert into inventory VALUES(0007, 'folder', 0.99, 374);
+insert into inventory VALUES(0008, 'binder', 6.99, 44);
+
+
+insert into employee VALUES('lisa', 1000, 'lisa@store.com', '123 road street Kent, OH 44240', 50000);
+insert into employee VALUES('bob', 1001, 'bob@store.com', '234 lane avenue streetsboro, OH 44241', 55000);
+insert into employee VALUES('sam', 1002, 'sam@store.com', '2897 park avenue Ravenna, OH 44266', 40000);
+insert into employee VALUES('kristen', 1003, 'kristen@store.com', '945 water street Kent, OH 44240', 60000);
+insert into employee VALUES('maggie', 1004, 'maggie@store.com', '9923 highway circle Kent, OH 44240', 75000);
+
+
+insert into customer VALUES(9001, 'jordan', 'jordan@kent.edu', '3091 jordan lane Ravenna, OH 44266', 3309887654);
+insert into customer VALUES(9002, 'lucy', 'lucy@kent.edu', '3463 lucy road Ravenna, OH 44266', 3304358856);
+insert into customer VALUES(9003, 'allan', 'allan@kent.edu', '1193 allan lane Kent, OH 44240', 4407585336);
+insert into customer VALUES(9004, 'mike', 'mike@kent.edu', '4659 ,mike avenue Hudson, OH 44236', 2165447258);
+insert into customer VALUES(9005, 'deanna', 'deanna@kent.edu', '6465 deanna circle Ravenna, OH 44266', 3305714698);
+
+
+insert into transactions VALUES(5001, 1555848228, 9001, 56.96, 'visa', 1003 );
+insert into transactions VALUES(5002, 1555938900, 9002, 4.55, 'cash', 1001 );
+insert into transactions VALUES(5003, 1555944300, 9001, 11.67, 'gift card', 1001 );
+insert into transactions VALUES(5004, 1555946154, 9004, 522.84, 'cash', 1002 );
+insert into transactions VALUES(5005, 1555847094, 9005, 19.47, 'visa', 1004 );
+insert into transactions VALUES(5006, 1555834424, 9004, 27.39, 'mastercard', 1003 );
+insert into transactions VALUES(5007, 1555927064, 9003, 116.88, 'mastercard', 1005 );
