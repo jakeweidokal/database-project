f7bbb4d80e8a5125e6812c7f208419c1ec7b2e39
adam ruyten <aruyten@gmail.com>
2019-04-30 00:00:45 -0400

    added basic file to show transactions

--- proj_tableshow.php
+++ proj_tableshow.php
@@ -79,6 +79,45 @@ function show_inventory($conn){
 	//$conn->close();
 }
 
+function show_transactions($conn){
+
+	//include "dbconnect.php";
+
+	$sql = "SELECT * FROM transactions";
+	$result = $conn->query($sql);
+
+	if ($result->num_rows > 0) {
+		
+		echo "<br><h3> Detailed Transactions<h3> <br>";
+		
+		echo '<table border>';
+		echo '<thead><tr>';
+		echo '<th>'."Transaction ID".'</th>'.'<th>'."Timestamp".'</th>'.'<th>'."Customer ID".'</th>'.'<th>'."Total Spent".'</th>'.'<th>'."Payment Method".'</th>'.'<th>'."employee ID".'</th>';
+		echo '</tr></thead>';
+		echo '<tbody>';
+
+		while($row = $result->fetch_assoc()) {
+			echo '<tr>';
+			echo "<td>" . $row["transactionID"]. "</td>";
+			echo "<td>" . $row["timestamp"]. "</td>";
+			echo "<td>" . $row["customerID"]. "</td>";
+			echo "<td>" . $row["totalSpent"]. "</td>";
+			echo "<td>" . $row["paymentMethod"]. "</td>";
+			echo "<td>" . $row["employeeID"]. "</td>";
+			echo '</tr>';
+		}
+		
+		echo '</tbody>';
+		echo '</table>';
+		
+		// output data of each row
+		
+		
+	} else {
+		echo "0 results";
+	}
+	//$conn->close();
+}
 
 function show_join($conn){
 
---
+++ proj_transactions.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html> 
+<body >
+<div style="height:900px; background-color: lightblue;" align="center">
+<br><br><br><br>
+
+
+<?php
+    require("proj_dbconnect.php");
+    require("proj_tableshow.php");
+    show_transactions($conn);
+?>
+
+
+
+<br><br><br><br>
+<hr width="50">
+<a href="index.html" style="color:red;font-weight:bold;">Home</a>
+<hr width="50">
+</div>
+</body> </html>
